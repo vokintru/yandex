28158c04538618c06b07e246136e3008593e8517
vokintru <aodplayer@bk.ru>
2023-11-27 18:18:23 +0300

    4

--- PyGame 1. Введение/Скринсейвер.py
+++ PyGame 1. Введение/Скринсейвер.py
@@ -10,11 +10,11 @@ x = 960
 y = 540
 
 
-def draw(x, y):
-    pygame.draw.rect(screen, pygame.Color('#ffcc00'), (x, y, 100, 100))
+def draw(x, y, hexadecimal):
+    pygame.draw.circle(screen, pygame.Color(hexadecimal), (x, y), 20)
 
 
-draw(x, y)
+draw(x, y, "#ffffff")
 
 running = True
 while running:
@@ -22,10 +22,19 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    x += random.randint(-4, 4)
-    y += random.randint(-4, 4)
+    x += random.randint(-10, 10)
+    y += random.randint(-10, 10)
+    if x <= 0:
+        x = 0
+    if x >= 1920:
+        x = 1920
+    if y <= 0:
+        y = 0
+    if y >= 1080:
+        y = 1080
     pygame.draw.rect(screen, pygame.Color('#000000'), (0, 0, 0, 0))
-    draw(x, y)
+    hexadecimal = ["#" + ''.join([random.choice('ABCDEF0123456789') for i in range(6)])]
+    draw(x, y, hexadecimal[0])
     pygame.display.flip()
 
 pygame.quit()
