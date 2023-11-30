a6653a5601cafc5a27fe216f4d91c871f97616d9
vokintru <aodplayer@bk.ru>
2023-11-30 17:36:49 +0300

    8

---
+++ PyGame 2. Игровой цикл. События/Шарики.py
@@ -0,0 +1,42 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((400, 500))
+pygame.display.set_caption('circles')
+FPS = 165  # число кадров в секунду
+clock = pygame.time.Clock()
+circles = []
+speed_x = []
+speed_y = []
+radius = 0
+
+
+def draw(pos, r):
+    pygame.draw.circle(screen, (255, 255, 0), pos, r)
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONUP:
+            circles.append(event.pos)
+            speed_x.append(2)
+            speed_y.append(2)
+            radius = 10
+            draw(event.pos, radius)
+
+    screen.fill((0, 0, 0))
+    for i in range(len(circles)):
+        pos_circle = circles[i]
+        if pos_circle[0] >= 400 or pos_circle[0] <= 0:
+            speed_x[i] = - speed_x[i]
+        if pos_circle[1] >= 500 or pos_circle[1] <= 0:
+            speed_y[i] = - speed_y[i]
+        circles[i] = pos_circle[0] + speed_x[i], pos_circle[1] - speed_y[i]
+        draw(circles[i], radius)
+    pygame.display.flip()
+    clock.tick(FPS)
+
+pygame.quit()
