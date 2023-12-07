2bc549de8ee3394a035a16d30646b64ef35611b1
vokintru <aodplayer@bk.ru>
2023-12-07 19:06:42 +0300

    1

--- PyGame 4. Классические игры на клетчатом поле/Линеечки.py
+++ PyGame 4. Классические игры на клетчатом поле/Линеечки.py
@@ -6,6 +6,7 @@ class Lines(Board):
     # создание поля
     def __init__(self, width, height):
         super().__init__(width, height)
+        self.red = (-1, -1)
 
     def render(self, screen):
         for y in range(self.height):
@@ -24,12 +25,34 @@ class Lines(Board):
                     x * self.cell_size + self.left, y * self.cell_size + self.top, self.cell_size,
                     self.cell_size), 1)
 
+    def has_path(self, start, finish):
+        """ волновой алгоритм"""
+        distance = [[100] * self.width for _ in range(self.height)]
+        distance[start[1]][start[0]] = 0
+        queue = [start]  # кортеж координат стартовой клетки
+        while queue:
+            x, y = queue.pop(0)  # удаляем первый в очереди
+            for dx, dy in (1, 0), (-1, 0), (0, 1), (0, - 1):
+                if x + dx < 0 or x + dx >= self.width or y + dy < 0 or y + dy >= self.height:
+                    continue
+                if self.board[y + dy][x + dx] == 0 and distance[y + dy][x + dx] == 100:
+                    distance[y + dy][x + dx] = distance[y][x] + 1
+                    queue.append((x + dx, y + dy))
+        return distance[finish[1]][finish[0]]
+
     def on_click(self, cell):
         print(cell)
+        if self.red != (-1, -1) and self.has_path(self.red, cell) == 100:
+            return
+        if self.red != (-1, -1) and self.has_path(self.red, cell) != 100:
+            self.board[self.red[0]][self.red[1]] = 0
+            self.red = (-1, -1)
+
         if self.board[cell[1]][cell[0]] % 2 == 0:
             self.board[cell[1]][cell[0]] = 1
         else:
             self.board[cell[1]][cell[0]] = 2
+            self.red = (cell[1], cell[0])
 
 
 def main():
