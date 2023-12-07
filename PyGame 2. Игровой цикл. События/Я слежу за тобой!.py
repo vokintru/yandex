0df78349bfbc871f859920bd840d42263f3a31f8
vokintru <aodplayer@bk.ru>
2023-12-07 17:08:09 +0300

    Исправление от Миши

--- PyGame 2. Игровой цикл. События/Я слежу за тобой!.py
+++ PyGame 2. Игровой цикл. События/Я слежу за тобой!.py
@@ -7,13 +7,20 @@ screen = pygame.display.set_mode(size)
 running = True
 count = 1
 color = pygame.Color("red")
+font = pygame.font.Font(None, 100)
+text = font.render('1', True, color)
+text_x = width // 2 - text.get_width() // 2
+text_y = height // 2 - text.get_height() // 2
+text_w = text.get_width()
+text_h = text.get_height()
+screen.blit(text, (text_x, text_y))
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.WINDOWHIDDEN:
+        if event.type == pygame.WINDOWMINIMIZED:
             count += 1
-        if event.type == pygame.WINDOWSHOWN:
+            screen.fill((0, 0, 0))
             font = pygame.font.Font(None, 100)
             text = font.render(str(count), True, color)
             text_x = width // 2 - text.get_width() // 2
