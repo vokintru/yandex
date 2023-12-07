d9bb13ff5941ebff540bfdf8487ec0d97edfcc2b
vokintru <aodplayer@bk.ru>
2023-12-07 17:52:17 +0300

    v2

--- PyGame 4. Классические игры на клетчатом поле/Дедушка сапёра.py
+++ PyGame 4. Классические игры на клетчатом поле/Дедушка сапёра.py
@@ -1,42 +1,6 @@
 import pygame
 import random
-#from board import Board
-
-class Board:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.board = [[0] * width for _ in range(height)]
-        self.left = 10
-        self.top = 10
-        self.cell_size = 30
-
-    def set_view(self, left, top, cell_size):
-        self.left = left
-        self.top = top
-        self.cell_size = cell_size
-
-    def render(self, screen):
-        color = pygame.Color('white')
-        for y in range(self.height):
-            for x in range(self.width):
-                coor = (x * self.cell_size + self.left, y * self.cell_size + self.top, self.cell_size, self.cell_size)
-                pygame.draw.rect(screen, color, coor, 1)
-
-    def get_cell(self, mouse_pos):
-        cell_x = (mouse_pos[0] - self.left) // self.cell_size
-        cell_y = (mouse_pos[1] - self.top) // self.cell_size
-        if cell_x < 0 or cell_x >= self.width or cell_y < 0 or cell_y >= self.height:
-            return None
-        return cell_x, cell_y
-
-    def on_click(self, cell):
-        pass
-
-    def get_click(self, mouse_pos):
-        cell = self.get_cell(mouse_pos)
-        if cell:
-            self.on_click(cell)
+from board import Board
 
 
 class Minesweeper(Board):
