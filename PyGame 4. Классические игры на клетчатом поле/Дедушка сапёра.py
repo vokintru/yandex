d4379e13f11216e90a033d23f514818516a4ce29
vokintru <aodplayer@bk.ru>
2023-12-07 18:33:54 +0300

    13

--- PyGame 4. Классические игры на клетчатом поле/Дедушка сапёра.py
+++ PyGame 4. Классические игры на клетчатом поле/Дедушка сапёра.py
@@ -4,73 +4,62 @@ from board import Board
 
 
 class Minesweeper(Board):
-    def __init__(self, width, height, need):
+    def __init__(self, width, height, mine):
         super().__init__(width, height)
-        self.board = [[-1] * width for inet in range(height)]
-        ter = 0
-        while ter < need:
-            x = random.randint(0, self.width - 1)
-            y = random.randint(0, self.height - 1)
-            if self.board[y][x] == -1:
+        self.mine = mine
+        self.board = [[-1] * self.width for _ in range(self.height)]
+        print(self.board)
+        while self.mine:
+            x = random.randrange(0, self.width)
+            y = random.randrange(0, self.height)
+            print()
+            if self.board[y][x] != 10:
                 self.board[y][x] = 10
-                ter += 1
+                self.mine -= 1
 
-    def open_click(self, cell):
+    def render(self, screen):
+        for y in range(self.height):
+            for x in range(self.width):
+                if self.board[y][x] == 10:
+                    pygame.draw.rect(screen, pygame.Color(0, 255, 0),
+                                     (x * self.cell_size + self.left, y * self.cell_size + self.top,
+                                      self.cell_size, self.cell_size))
+                if 0 <= self.board[y][x] <= 8:
+                    font = pygame.font.Font(None, 25)
+                    text = font.render(f"{self.board[y][x]}", True, (100, 255, 100))
+                    screen.blit(text, (x * self.cell_size + self.left, y * self.cell_size + self.top))
+
+                pygame.draw.rect(screen, pygame.Color("white"), (
+                    x * self.cell_size + self.left, y * self.cell_size + self.top, self.cell_size,
+                    self.cell_size), 1)
+
+    def open_cell(self, cell):
         x, y = cell
-        if self.board[y][x] == 10:
-            return
         s = 0
         for dy in range(-1, 2):
             for dx in range(-1, 2):
                 if x + dx < 0 or x + dx >= self.width or y + dy < 0 or y + dy >= self.height:
                     continue
                 if self.board[y + dy][x + dx] == 10:
-                    s += 1
-        if s == 0:
-            s1 = 0
-            for x1 in range(self.width):
-                for y1 in range(self.height):
-                    s1 = 0
-                    if self.board[y1][x1] == -1:
-                        for dy1 in range(-1, 2):
-                            for dx1 in range(-1, 2):
-                                if x1 + dx1 < 0 or x1 + dx1 >= self.width or y1 + dy1 < 0 or y1 + dy1 >= self.height:
-                                    continue
-                                if self.board[y1 + dy1][x1 + dx1] == 10:
-                                    s1 += 1
-                        if s1 == 0:
-                            self.board[y1][x1] = s1
-        self.board[y][x] = s
+                    s += self.board[y + dy][x + dx]
+        self.board[y][x] = s // 10
+
 
     def on_click(self, cell):
-        self.open_click(cell)
-
-    def render(self, screen):
-        color = pygame.Color('red')
-        for y in range(self.height):
-            for x in range(self.width):
-                coor = (x * self.cell_size + self.left, y * self.cell_size + self.top,
-                        self.cell_size, self.cell_size)
-                coor1 = (x * self.cell_size + self.left + 2, y * self.cell_size + self.top + 2,
-                         self.cell_size, self.cell_size)
-                if self.board[y][x] == 10:
-                    pygame.draw.rect(screen, color, coor)
-                pygame.draw.rect(screen, pygame.Color('white'), coor, 1)
-                if self.board[y][x] >= 0 and self.board[y][x] != 10:
-                    font = pygame.font.Font(None, self.cell_size - 7)
-                    text = font.render(str(self.board[y][x]), 1, (100, 255, 100))
-                    screen.blit(text, coor1)
+        print(cell)
+        self.open_cell(cell)
 
 
 def main():
     pygame.init()
     size = 500, 500
     screen = pygame.display.set_mode(size)
-    pygame.display.set_caption('Папа сапёра')
+    pygame.display.set_caption('Дедушка сапёра')
     clock = pygame.time.Clock()
     board = Minesweeper(10, 15, 10)
     ticks = 0
     running = True
+    FPS = 60
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -80,7 +69,7 @@ def main():
         screen.fill((0, 0, 0))
         board.render(screen)
         pygame.display.flip()
-        clock.tick(50)
+        clock.tick(FPS)
         ticks += 1
     pygame.quit()
 
