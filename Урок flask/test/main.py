e70cfb5f9a2c234e2a8697146328d8aedd9ebfeb
vokintru <aodplayer@bk.ru>
2024-02-08 18:05:38 +0300

    3

--- Урок flask/test/main.py
+++ Урок flask/test/main.py
@@ -19,5 +19,12 @@ def promotion():
            '<p>Мы сделаем обитаемыми безжизненные пока планеты.</p><p>И начнем с Марса!</p><p>Присоединяйся!</p>'
 
 
+@app.route("/image_mars")
+def image_mars():
+    return '<head><title>Привет, Марс!</title></head><body><p>Жди нас, Марс!</p><img src="static/MARS.png" ' \
+           'alt="здесь должна была быть картинка, но не нашлась">' \
+           '<p>Вот она какая, красная планета</p></body>'
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
